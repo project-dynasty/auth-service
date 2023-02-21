c5419b52066d6a1a8e2a43276961b736a3fabe10
Alexander Wodarz <git@alexanderwodarz.de>
2023-02-21 11:10:37 +0100

    Add screenSize as property

--- src/main/java/com/projectdynasty/payload/request/SigninRequest.java
+++ src/main/java/com/projectdynasty/payload/request/SigninRequest.java
@@ -8,6 +8,6 @@ import lombok.Getter;
 public class SigninRequest {
     private final String username;
     private final String password;
-    private final String osType, osVersion;
+    private final String osType, osVersion, screenSize;
     private final boolean rememberMe;
 }
